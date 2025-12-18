179e1280e2f11f3f504fa4f798a1732fccc2f2d7
Omar <omar.hammodan@gmail.com>
2025-12-17 20:19:56 -0500

    refactored code, changed terminal escape to ctrl+q

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+// Macro for mapping ctrl+key (ctrl+a=1, ctrl+b=2, etc)
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 /*** terminal ***/
@@ -51,25 +54,36 @@ void enableRawMode()
         die("tcsetattr");
 }
 
-/*** init ***/
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+/*** input ***/
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
 int main()
 {
     enableRawMode();
     while (1)
     {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-            die("read");
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == 'q')
-            break;
+        editorProcessKeypress();
     }
     return 0;
 }
