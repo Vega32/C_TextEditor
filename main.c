fd3b07438bf676dd9516d1261ee0bbee6e238e4c
Omar <omar.hammodan@gmail.com>
2026-01-01 13:57:33 -0500

    added welcome message to center of terminal

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <string.h>
 
+#define VERSION "0.0.1"
 // Macro for mapping ctrl+key (ctrl+a=1, ctrl+b=2, etc)
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -148,8 +149,28 @@ void editorDrawRows(struct abuf *ab)
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "EDITOR -- VERSION %s", VERSION);
+            if (welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
 
+        abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1)
         {
             abAppend(ab, "\r\n", 2);
@@ -160,12 +181,14 @@ void editorDrawRows(struct abuf *ab)
 void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
-    abAppend(&ab, "\x1b[2J", 4);
+
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
